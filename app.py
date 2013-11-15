27651a29febbe93167b2be0ba89b6825c021e2b2
Javiera Guedes <jguedes@dhcp-192-033-102-090.ethz.ch>
2013-11-15 15:12:30 +0100

    deleted some old files

--- app.py
+++ app.py
@@ -8,18 +8,19 @@ app.config.update(
 )
 
 # controllers
-@app.route("/")
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
+@app.route("/")
 def index():
 	return render_template('index.html')
 
 # launch
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
