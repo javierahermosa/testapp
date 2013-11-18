714924abbad6b1f8658fc588e5b4a40321914412
Javiera Guedes <jguedes@dhcp-192-033-102-090.ethz.ch>
2013-11-18 15:32:41 +0100

    implemented basic features

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, flash
 
 # initialization
 app = Flask(__name__)
 app.config.update(
     DEBUG = True
 )
+app.secret_key = "talca"
 
 # controllers
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_direcory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -20,7 +21,13 @@ def page_not_found(e):
 def index():
 	return render_template('index.html')
 
+@app.route('/',methods=['GET','POST'])
+def post():
+	result = request.form['expression']
+	flash(result)
+	return index()
+
 # launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
